fb753c2d5d9e82d65b57b3337ff81ef05f00eca3
Wonjong Park <wonjong2@gmail.com>
2022-03-05 21:34:10 -0800

    Updated the 1st implementation

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// pre-defined arrays for alphabet(lower/upper) and special characters
-const alphabet = [['a', 'A'], ['b', 'B'], ['c', 'C'], ['d', 'D'], ['e', 'E'], ['f', 'F'], ['g', 'G'], ['h', 'H'], ['i', 'I'], ['j', 'J'], ['k', 'K'], ['l', 'L'], ['m', 'M'], ['n', 'N'], ['o', 'O'], ['p', 'P'], ['q', 'Q'], ['r', 'R'], ['s', 'S'], ['t', 'T'], ['u', 'U'], ['v', 'V'], ['w', 'W'], ['x', 'X'], ['y', 'Y'], ['z', 'Z']];
-const specialChar = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
-
 // Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-// Generate random character according to 'policy' which is character type
-function getRandomChar(policy) {
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// pre-defined arrays for alphabet(lower/upper) and special characters
+var alphabet = [['a', 'A'], ['b', 'B'], ['c', 'C'], ['d', 'D'], ['e', 'E'], ['f', 'F'], ['g', 'G'], ['h', 'H'], ['i', 'I'], ['j', 'J'], ['k', 'K'], ['l', 'L'], ['m', 'M'], ['n', 'N'], ['o', 'O'], ['p', 'P'], ['q', 'Q'], ['r', 'R'], ['s', 'S'], ['t', 'T'], ['u', 'U'], ['v', 'V'], ['w', 'W'], ['x', 'X'], ['y', 'Y'], ['z', 'Z']];
+var specialChar = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+// Generate one random character
+function generateRandomChar(typeOfChar) {
   // generate random number(1~100)
-  let randomVal = Math.floor(Math.random() * 100) + 1;
+  var randomVal = Math.floor(Math.random() * 100) + 1;
 
-  switch(policy){
+  switch(typeOfChar){
     case "UC":  // Uppercase Character
       return alphabet[randomVal%26][1];
     case "SC":  // Special Character
@@ -26,13 +36,11 @@ function getRandomChar(policy) {
 
 function generatePassword() {
   // Variable for the generated pwd
-  let generatedPassword = ""; 
-
-  // let appliedPolicy = false;
-  let policyIndex = 0;
+  var generatedPassword = ""; 
+  var typeOfChar = 0;
 
   // this array will be updated with answer from user
-  let passwordPolicy = [];
+  var passwordPolicy = [];
 
   // get the number of characters in password (range : 8~128)
   passwordPolicy[0] = prompt("How Many characters would you like your password to contain?", "8 ~ 128");
@@ -41,60 +49,53 @@ function generatePassword() {
     passwordPolicy[0] = prompt("Sorry, your answer is invalid. How Many characters would you like your password to contain?", "8 ~ 128");    
   }
 
-  // password will include special character?
-  if (confirm("Click OK to confirm including special characters.")){
-    passwordPolicy.push(["SC", false]);
-  }
+  do {
+    // password will include Lowercase character?
+    if (confirm("Click OK to confirm including lowercase characters.")){
+      passwordPolicy.push(["LC", "Not Included"]);
+    }
 
-  // password will include numeric character?
-  if (confirm("Click OK to confirm including numeric characters.")) {
-    passwordPolicy.push(["NC", false]);
-  }
+    // password will include uppercase character?
+    if (confirm("Click OK to confirm including uppercase characters."))
+    {
+      passwordPolicy.push(["UC", "Not Included"]);
+    }    
 
-  // password will include uppercase character?
-  if (confirm("Click OK to confirm including uppercase characters."))
-  {
-    passwordPolicy.push(["UC", false]);
-  }
+    // password will include numeric character?
+    if (confirm("Click OK to confirm including numeric characters.")) {
+      passwordPolicy.push(["NC", "Not Included"]);
+    }
 
-  // for(let index = 0; index < passwordPolicy.length; index++) {
-  //   appliedPolicy &= passwordPolicy[index][1];
-  // }
+    // password will include special character?
+    if (confirm("Click OK to confirm including special characters.")){
+      passwordPolicy.push(["SC", "Not Included"]);
+    }
+
+    // validate if user selected at least one character type or not.
+    if (passwordPolicy.length === 1) {
+      confirm("You have to select at least one character type to be included in password. Please check again.")
+    }
+  } while (passwordPolicy.length === 1)
 
   // generate a new password based on user's answer
-  for (let index1 = 0; index1 < passwordPolicy[0]; index1++) {
+  for (var i = 0; i < passwordPolicy[0]; i++) {
+    // generate one random character
+    typeOfChar = ((Math.floor(Math.random() * 100) + 1)%(passwordPolicy.length-1)) + 1;
+    generatedPassword += generateRandomChar(passwordPolicy[typeOfChar][0]);
+    passwordPolicy[typeOfChar][1] = "Included";
 
-    // if(((passwordPolicy[0]-index1) === (passwordPolicy.length-1)) && !appliedPolicy) {
-    //   for(let index2 =0; index2 < passwordPolicy.length; index2) {
-    //     if(passwordPolicy[index2][1] == false){
-    //       generatedPassword += getRandomChar(passwordPolicy[index2]);
-    //       passwordPolicy[index2][1] = true;
-    //     }
-    //   }
-    //   return generatedPassword;
-    // }
-
-    policyIndex = (Math.floor(Math.random() * 100) + 1)%passwordPolicy.length;
-    if (policyIndex === 0) {
-      generatedPassword += getRandomChar("LC");
-    }
-    else {
-      generatedPassword += getRandomChar(passwordPolicy[policyIndex][0]);
-      // passwordPolicy[policyIndex][1] = true;
+    // check wheather user selected character types are inlcuded in password or not
+    // if there are any missing character types in password, force them to be included. 
+    if (passwordPolicy.length === (passwordPolicy[0] - i)) {
+      for (var j = 1; j < passwordPolicy.length; j++) {
+        if (passwordPolicy[j][1] === "Not Included") {
+          generatedPassword += generateRandomChar(passwordPolicy[j][0]);
+          passwordPolicy[j][1] = "Included";
+          i++;
+        }
+      }
     }
   }
 
-  return generatedPassword;
+  return generatedPassword; // return generated password
 }
-
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-//
